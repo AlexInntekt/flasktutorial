84e76f9d82449c8f63153db7c7c6c0aeeb9bd0bb
alex <alex.inntekt@gmail.com>
2020-06-23 23:52:08 +0300

    validation and error messages for login form

--- base/routes.py
+++ base/routes.py
@@ -18,7 +18,11 @@ def index():
     ]
     return render_template('index.html', title='Index home page', user=user, posts=posts)
 
-@base.route('/login')
+@base.route('/login', methods=['GET', 'POST'])
 def login():
     form = LoginForm()
+    if form.validate_on_submit():
+        flash('Login requested for user {}, remember_me={}'.format(
+            form.username.data, form.remember_me.data))
+        return redirect('/index')
     return render_template('login.html', title='Sign In', form=form)
