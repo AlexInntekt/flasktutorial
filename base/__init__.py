766ec1ec5934aeb709e449b67a3ef9412e266701
alex <alex.inntekt@gmail.com>
2020-06-24 00:20:04 +0300

    working on the database!

--- base/__init__.py
+++ base/__init__.py
@@ -1,7 +1,12 @@
 from flask import Flask
 from config import Config
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 base = Flask(__name__)
 base.config.from_object(Config)
+db = SQLAlchemy(base)
+migrate = Migrate(base, db)
 
-from base import routes
+
+from base import routes, models
