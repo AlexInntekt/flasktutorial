3c2324cf94adce733e0cba3bd3de5bc83c11c0b7
alex <alex.inntekt@gmail.com>
2020-06-24 00:26:33 +0300

    added in the context application shell: db User and Post

--- app.py
+++ app.py
@@ -1,1 +1,6 @@
-from base import base
+from base import base, db
+from base.models import User, Post
+
+@base.shell_context_processor
+def make_shell_context():
+    return {'db': db, 'User': User, 'Post': Post}
