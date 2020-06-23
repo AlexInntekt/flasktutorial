084b68e6bcf04641ea26ff2c689b11406342ad12
alex <alex.inntekt@gmail.com>
2020-06-23 23:27:51 +0300

    hardcoded some posts!

--- base/routes.py
+++ base/routes.py
@@ -1,7 +1,24 @@
 from flask import render_template
 from base import base
+from base.forms import LoginForm
 
 @base.route('/')
 @base.route('/index')
 def index():
-    return render_template('index.html', title='Index home page')
+    user = {'username':'Alex_the_brave447'}
+    posts = [
+        {
+            'author': {'username': 'John'},
+            'body': 'Beautiful day in Portland!'
+        },
+        {
+            'author': {'username': 'Susan'},
+            'body': 'The Avengers movie was so cool!'
+        }
+    ]
+    return render_template('index.html', title='Index home page', user=user, posts=posts)
+
+@base.route('/login')
+def login():
+    form = LoginForm()
+    return render_template('login.html', title='Sign In', form=form)
